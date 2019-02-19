d4d208f42976f4ea41645d4b2dc9060164dcbab4
Caio Cesar <caio.teixeira@educadventista.org.br>
2019-02-19 14:57:51 -0300

    alterações precisas

---
+++ precache-manifest.0024e6e9ab8d93a1215e315386a3db05.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0344e6b9d54d4d18051",
+    "url": "/js/about.20624161.js"
+  },
+  {
+    "revision": "4ad8aee238fdbe0f6202",
+    "url": "/css/app.7b92febe.css"
+  },
+  {
+    "revision": "4ad8aee238fdbe0f6202",
+    "url": "/js/app.ca7570b1.js"
+  },
+  {
+    "revision": "f33d3036ddfc53e58681",
+    "url": "/css/chunk-vendors.fae5b87b.css"
+  },
+  {
+    "revision": "f33d3036ddfc53e58681",
+    "url": "/js/chunk-vendors.bdef19be.js"
+  },
+  {
+    "revision": "95ed826080ea051282b7360630f900b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "004082c8e33dfd9bc15be7ebc5f15dd7",
+    "url": "/404.html"
+  },
+  {
+    "revision": "9b51a2ff77bb4bfc6af446faae567fa1",
+    "url": "/images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "5d69bee7059764593f0e892a0cd30a31",
+    "url": "/images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "fb1ab46a58be914cf212684972707ec2",
+    "url": "/images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "b6ec0da8aacd54189f30ce32ed1db841",
+    "url": "/images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "568ab5b5e6dbe76e210b69db5f3f8c0f",
+    "url": "/images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "6fd6ffb56cc9184e2a5d4046a83f5b8a",
+    "url": "/images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "568ab5b5e6dbe76e210b69db5f3f8c0f",
+    "url": "/images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "568ab5b5e6dbe76e210b69db5f3f8c0f",
+    "url": "/images/icons/icon-512x512.png"
+  }
+];
